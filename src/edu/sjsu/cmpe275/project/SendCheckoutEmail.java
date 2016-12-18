2884ec771f6ed91b172973b37c628495993a714e
siddharthgupta <siddharth6258@gmail.com>
2016-12-17 21:11:20 -0800

    commit

--- src/edu/sjsu/cmpe275/project/SendCheckoutEmail.java
+++ src/edu/sjsu/cmpe275/project/SendCheckoutEmail.java
@@ -149,5 +149,47 @@ public static void checkoutFine(User user,Book book,Checkout checkout) {
 		}
 		
 	}
+public static void returnReminder(User user,Book book,Checkout checkout,int diffInDays) {
+	
+	final String username = "ajaymodi12@gmail.com";
+	final String password = "pradeep";
+	Properties props = new Properties();
+	props.put("mail.smtp.auth", "true");
+	props.put("mail.smtp.starttls.enable", "true");
+	props.put("mail.smtp.host", "smtp.gmail.com");
+	props.put("mail.smtp.port", "587");
+	
+	Calendar cal=Calendar.getInstance();
+	Calendar cal1=Calendar.getInstance();
+	cal1.add(Calendar.DATE, 30);
+	
+	Session session = Session.getInstance(props,
+			  new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			  });
+	
+	try {
+		diffInDays=diffInDays*(-1);
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(username));
+		message.setRecipients(Message.RecipientType.TO,
+			InternetAddress.parse(user.getEmail()));
+		message.setSubject("Return Book within "+diffInDays+" Days");
+		message.setText("Dear User,"
+			+ "\n\n Return Book within "+diffInDays+" Days"
+				+ "\n\nBook Name: "+book.getTitle()
+				+ "\nBook Author: "+book.getAuthor());
+
+		Transport.send(message);
+
+		System.out.println("Done");
+
+	} catch (MessagingException e) {
+		throw new RuntimeException(e);
+	}
+	
+}
 
 }
